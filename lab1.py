90795d39fa4ae768d7ce91e782faeebe1e111d4a
Lumax877 <73527281+Lumax877@users.noreply.github.com>
2021-10-13 19:01:19 +0200

    weekly środa wieczur coding

---
+++ lab1.py
@@ -0,0 +1,81 @@
+# zad 1
+# def funkcja(a,b):
+#     return print(a + "." + b)
+# a = 'J'
+# b = 'Kowalski'
+# funkcja(a,b)
+
+# zad 2
+# def funkcja2(a,b):
+#     return print(a[0] + "." + b)
+# a = "Jan"
+# b = "Kowalski"
+# funkcja2(a,b)
+
+# zad 3
+# def rok(a,b,c):
+#     return a * 100 + b - c
+# a = 20
+# b = 21
+# c = 21
+# rok(a,b,c)
+
+# zad 4
+# def skrot(a,b,c):
+#     return c
+# skrot(a,b,funkcja2(a,b))
+
+# zad 5
+# a = 5.0
+# b = 3.0
+# def dziel(a,b):
+#     if (a > 0 and b > 0):
+#         return print(a/b)
+#     return 0
+# dziel(a,b)
+
+# zad 6
+# suma = 0
+# while 1:
+#     x = input("daj liczbe ")
+#     x = int(x)
+#     suma = suma + x
+#     if suma > 100 :
+#         print(suma)
+#         break
+
+# zad 7
+# def druk(lista):
+# #     for x in lista:
+# #         print(x)
+# #     return 0
+# # lista = [2,3,4,5,6,7]
+# # druk(lista)
+
+# zad 8
+# lista = []
+# def putin(lista):
+#     for x in range(3):
+#         x = input("wpisz cos")
+#         lista.append(x)
+#     print(lista)
+#     return 0
+# putin(lista)
+
+# zad 9
+# tydzien = ['pon', 'wt', 'sr', 'czw', 'pt', 'sob', 'ndz']
+# def mittwoch(a):
+#    return print(tydzien[a-1])
+# mittwoch(3)
+
+# zad 10
+# def palin(a):
+#     for x in range(1,len(a) + 1):
+#         if a[x - 1] != a[len(a) - x]:
+#             return print('nie')
+#     return print('tak')
+# a = 'madam'
+# palin(a)
+
+
+
